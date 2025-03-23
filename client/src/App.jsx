d96b055d0714934434e788b82f0c752bffbe9232
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2025-03-23 17:09:09 +0530

    fix: basic chat ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect, useState } from "react";
+import { MicIcon, MicOffIcon, Send } from "lucide-react";
 import { WS_ENDPOINT } from "./constants";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   const isPlayingRef = useRef(false);
   const [ws, setWs] = useState(null);
   const [isRecording, setIsRecording] = useState(false);
+  const [chatMessages, setChatMessages] = useState([]);
   const mediaRecorderRef = useRef(null);
   const uuidRef = useRef(null);
 
@@ -75,6 +77,15 @@ function App() {
         console.log("debug> Receiving server message:", event.data);
         try {
           const message = JSON.parse(event.data);
+          if (message.type === "transcript_item") {
+            setChatMessages((prevMessages) => [
+              ...prevMessages,
+              {
+                query: message["transcript_item"].query,
+                response: message["transcript_item"].response,
+              },
+            ]);
+          }
           console.log({ message });
         } catch (e) {
           console.log("Non-JSON message:", e);
@@ -167,40 +178,61 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col max-w-sm mx-auto pt-10 gap-4">
-      <input
-        className="border border-gray-500 rounded px-2 py-1"
-        ref={inputRef}
-        placeholder="Enter text to synthesize"
-      />
-
-      <button
-        className="bg-gray-800 text-white w-max px-2 py-1 mx-auto rounded"
-        onClick={() => {
-          const content = inputRef.current.value.trim();
-          if (content && ws && ws.readyState === 1) {
-            console.log("debug> Sending ws message");
-            ws.send(JSON.stringify({ type: "text", text: content }));
-          }
-        }}
+    <div className="flex flex-col h-screen overflow-hidden mx-auto bg-blue-100 gap-4">
+      {/* Chat */}
+      <div
+        style={{ height: "calc(100vh - 64px" }}
+        className="max-w-2xl mx-auto w-full overflow-y-auto"
       >
-        Send
-      </button>
+        {chatMessages.map((message, index) => (
+          <div key={index} className="p-2 rounded">
+            <p className="text-sm text-white bg-blue-400 mb-2 w-max px-2 ml-auto py-1 rounded max-w-sm">
+              {message.query}
+            </p>
 
-      <button
-        className={`${
-          isRecording ? "bg-red-600" : "bg-gray-800"
-        } text-white w-max px-2 py-1 rounded mx-auto`}
-        onClick={() => {
-          if (isRecording) {
-            stopRecording();
-          } else {
-            startRecording();
-          }
-        }}
-      >
-        {isRecording ? "Stop Recording" : "Start Recording"}
-      </button>
+            <p className="text-sm text-white bg-blue-500 mb-2 w-max px-2 py-1 rounded max-w-sm">
+              {message.response}
+            </p>
+          </div>
+        ))}
+      </div>
+      {/* Footer */}
+      <div className="fixed bottom-0 w-full max-w-2xl px-4 pb-1.5 left-1/2 -translate-x-1/2 flex items-center gap-2">
+        <textarea
+          className="border resize-none border-gray-500 rounded px-2 py-1 flex-1 left-0"
+          ref={inputRef}
+          placeholder="Chat with your agent"
+        />
+
+        <button
+          className="bg-gray-800 text-white w-max p-2 rounded-full"
+          onClick={() => {
+            const content = inputRef.current.value.trim();
+            if (content && ws && ws.readyState === 1) {
+              console.log("debug> Sending ws message");
+              ws.send(JSON.stringify({ type: "text", text: content }));
+              inputRef.current.value = "";
+            }
+          }}
+        >
+          <Send size={18} />
+        </button>
+
+        <button
+          className={`${
+            isRecording ? "bg-red-600" : "bg-gray-800"
+          } text-white w-max p-2 rounded-full`}
+          onClick={() => {
+            if (isRecording) {
+              stopRecording();
+            } else {
+              startRecording();
+            }
+          }}
+        >
+          {isRecording ? <MicIcon size={18} /> : <MicOffIcon size={18} />}
+        </button>
+      </div>
     </div>
   );
 }
